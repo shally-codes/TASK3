ac81e9081ef184325eaa64adc343082862099a51
shally-codes <shallymaurya1912003@gmail.com>
2025-08-10 16:23:45 +0530

    Add files via upload

---
+++ TASK 3.sql
@@ -0,0 +1,42 @@
+/*CREATE TABLE employee1 (
+	Employees_id INTEGER PRIMARY KEY,
+	First_name VARCHAR(50),
+	Last_name VARCHAR(50),
+	Department VARCHAR(50),
+	Salary DECIMAL(10,2)
+
+
+
+INSERT INTO employee1 (Employees_id, First_name, Last_name, Department, Salary)
+VALUES
+(1, 'Aman', 'maurya', 'Account', 50000),
+(2, 'Anshu', 'Sharma', 'IT', 50000),       
+(3, 'Ansh', 'Maurya', 'HR', 45000),           
+(4, 'Gungun ', 'IT', 'Finance', 60000),
+(5, 'Vaibhav', 'Maurya', 'IT', 48000);  
+
+
+SELECT first_name, last_name, salary FROM employee1 
+
+
+
+SELECT * FROM employee1
+WHERE department = 'it' 
+
+
+SELECT * FROM employee1
+WHERE salary = 50000
+
+SELECT first_name, last_name, salary
+FROM employee1
+ORDER BY salary DESC 
+
+*/
+
+
+
+
+
+
+ SELECT * FROM employee1   
+ 
